0d1c9b27cae3250304afe8c7e002a7072c0690ce
Elise Maigné <elise.maigne@inrae.fr>
2020-09-11 12:06:15 +0200

    help filterSmallChromosomes

--- R/filterSmallChromosomes.R
+++ R/filterSmallChromosomes.R
@@ -1,21 +1,32 @@
+#' Suppress the small chromosomes
+#'
+#' The function return an HiCDOCExp object, with only the big chromosomes.
+#' The big chromosomes must have a totalBins > object@minLength.
+#' The interactions are reduced to keep only those corresponding 
+#' to the remaining chromosomes 
+#' 
+#' @param object A HiCDOC object
+#'
+#' @return A HiCDOC object
 #' @export
+#'
+#' @examples 
+#' object <- HiCDOCExample()
+#' object@chromosomes
+#' object <- filterSmallChromosomes(object)
+#' object@chromosomes
+
 filterSmallChromosomes <- function(object) {
 
-  bigChromosomes <- fullInteractions(object) %>%
-    select(chromosome, `position.1`) %>%
-    distinct() %>%
-    group_by(chromosome) %>%
-    summarize(totalBins = n()) %>%
-    ungroup() %>%
-    filter(totalBins >= object@minLength) %>%
-    pull(chromosome) %>%
-    as.character()
+  bigChromosomes <- vapply(object@totalBins, function(x) x >= object@minLength, FUN.VALUE = TRUE)
+  bigChromosomes <- names(bigChromosomes)[bigChromosomes == TRUE]
+  bigChromosomes <- mixedsort(bigChromosomes)
 
   object@interactions %<>%
     filter(chromosome %in% bigChromosomes) %>%
     mutate(chromosome = factor(chromosome))
 
-  object@chromosomes <- mixedsort(bigChromosomes)
+  object@chromosomes <- bigChromosomes
   object@totalBins <- object@totalBins[object@chromosomes]
   object@weakBins  <- object@weakBins[object@chromosomes]
 
