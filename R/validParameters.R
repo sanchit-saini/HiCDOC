2eae2165001a9ba4b8870c9c68fd6eb9357de54f
Elise Maigné <elise.maigne@inrae.fr>
2020-09-11 12:07:19 +0200

    Add testSlotsHiCDOCExp function

--- R/validParameters.R
+++ R/validParameters.R
@@ -20,3 +20,40 @@ testchromosome <- function(object, chrId){
   }
   return(chr)
 }
+
+
+#' Test the existence of slots in HiCDOCExp object
+#'
+#' @param object a R object
+#' @param slots the names of the slots to test the existence
+#'
+#' @return An error if the object is not a HiCDOCExp object or
+#' a slot is missing, NULL otherwise
+testSlotsHiCDOCExp <- function(object, slots = NULL){
+  if(class(object) != "HiCDOCExp"){
+    stop("The object provided is not from class HiCDOCExp", call.=FALSE)
+  }
+  if(is.null(slots) == F){
+    existing <- slotNames(object)
+    existing <- existing[vapply(existing, function(x) !is.null(slot(object, x)), TRUE)]
+    missing <- slots[!(slots %in% existing)]
+    if(length(missing) > 0){
+      for(i in missing){
+        if(i == "interactions"){
+          message(paste0(
+            "Interaction matrix is not loaded yet. ",
+            "Please provide an HiCDOC object, with a non empty slot interactions."
+          ))
+        }
+        if(i %in% c("concordances", "compartments", "differences")){
+          message(paste0(
+            i, " are not computed. Please run 'detectCompartments' first."
+          ))
+        }
+      }
+      stop(paste("missing slot(s):", paste(missing, collapse = ", ")), call. = FALSE)
+    }
+  } else {
+    return(NULL)
+  }
+}
