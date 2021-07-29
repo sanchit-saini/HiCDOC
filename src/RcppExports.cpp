4953805989a3a0e6f49d2493cf4788faae714d6d
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-29 17:40:36 +0200

    Comply with BioCCheck.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // constrainedClustering
 List constrainedClustering(NumericMatrix& rMatrix, IntegerMatrix& rLinks, double maxDelta, int maxIterations, int totalRestarts, int totalClusters);
 RcppExport SEXP _HiCDOC_constrainedClustering(SEXP rMatrixSEXP, SEXP rLinksSEXP, SEXP maxDeltaSEXP, SEXP maxIterationsSEXP, SEXP totalRestartsSEXP, SEXP totalClustersSEXP) {
