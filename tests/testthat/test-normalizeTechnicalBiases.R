1d560d72922ad603a39ff2ed33590b0d7da409a5
Elise Maigné <elise.maigne@inrae.fr>
2021-08-24 14:32:24 +0200

    remove parallel testing (not useful)

--- tests/testthat/test-normalizeTechnicalBiases.R
+++ tests/testthat/test-normalizeTechnicalBiases.R
@@ -19,24 +19,3 @@ test_that("normalizeTechnicalBiases behaves as expected", {
     expect_equal(mean(object@interactions$interaction), 343.0371,
                  tolerance = 1e-4)
 })
-
-
-test_that("normalizeTechnicalBiases behaves as expected in parallel", {
-    multiParam <- BiocParallel::MulticoreParam(workers = 3)
-    BiocParallel::register(multiParam, default = TRUE)
-    # Apply normalization
-    object <- normalizeTechnicalBiases(object, parallel = TRUE)
-    # Keep object format
-    expect_is(object@interactions$chromosome, "factor")
-    expect_is(object@interactions$bin.1, "integer")
-    expect_is(object@interactions$bin.2, "integer")
-    expect_is(object@interactions$condition, "factor")
-    expect_is(object@interactions$replicate, "factor")
-    expect_is(object@interactions$interaction, "numeric")
-    # Keep 0 values before normalisation
-    expect_equal(nrow(object@interactions), 181566)
-    expect_equal(mean(object@interactions$bin.1), 51.43243, tolerance = 1e-4)
-    expect_equal(mean(object@interactions$bin.2), 101.6621, tolerance = 1e-4)
-    expect_equal(mean(object@interactions$interaction), 343.0371,
-                 tolerance = 1e-4)
-})
