2b551d00c29b2a8754f24eed5d9b0b47cdb765bb
Elise Maigné <elise.maigne@inrae.fr>
2021-12-10 11:42:55 +0100

    add packages into {HiCDOC}

--- R/HiCDOC-package.R
+++ R/HiCDOC-package.R
@@ -4,12 +4,15 @@
 #' @import methods
 #' @import zlibbioc
 #' @import ggplot2
-#' @importFrom GenomicRanges start end distance
+#' @importFrom GenomicRanges start end distance GRanges
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
 #' @importFrom Rcpp evalCpp sourceCpp
 #' @importFrom stats D loess median quantile
+#' @importFrom S4Vectors DataFrame mcols
+#' @importFrom GenomeInfoDb seqlevels
 #' @import data.table
+#' @import InteractionSet
 #' @useDynLib HiCDOC
 #' @aliases HiCDOC-package
 "_PACKAGE"
