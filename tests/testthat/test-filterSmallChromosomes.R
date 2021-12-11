53375eb535be8875c3f1503758f83c54a4c55080
Elise Maigné <elise.maigne@inrae.fr>
2021-12-11 15:58:18 +0100

    passing tests on filterSmall

--- tests/testthat/test-filterSmallChromosomes.R
+++ tests/testthat/test-filterSmallChromosomes.R
@@ -7,7 +7,7 @@ test_that("filterSmallChromosomes behave as expected with default filter", {
         "Keeping chromosomes with at least 100 positions."
     )
     expect_equal(length(object@chromosomes), 3)
-    expect_equal(nrow(object@interactions), 166150)
+    expect_equal(nrow(object), 40240)
 })
 
 test_that("filterSmallChromosomes behave as expected with custom filter", {
@@ -16,8 +16,7 @@ test_that("filterSmallChromosomes behave as expected with custom filter", {
         object <- filterSmallChromosomes(exampleHiCDOCDataSet, 161),
         "Keeping chromosomes with at least 161 positions."
     )
-    expect_equal(length(object@chromosomes), 1)
     expect_identical(object@chromosomes, "Z")
-    expect_equal(nrow(object@interactions), 40200)
+    expect_equal(nrow(object), 20100)
     expect_identical(object@parameters$smallChromosomeThreshold, 161)
 })
