133d2ad2c13ae8601c1e635216181e665803a093
Elise Maigné <elise.maigne@inrae.fr>
2021-12-17 14:56:01 +0100

    test normalized

--- tests/testthat/test-normalizeTechnicalBiases.R
+++ tests/testthat/test-normalizeTechnicalBiases.R
@@ -5,18 +5,12 @@ test_that("normalizeTechnicalBiases behaves as expected", {
     object <- filterWeakPositions(object)
     
     # Apply normalization
-    expect_warning(object <- normalizeTechnicalBiases(object, parallel = FALSE))
+    expect_warning(norm <- normalizeTechnicalBiases(object, parallel = FALSE))
     # Keep object format
-    expect_is(object@interactions$chromosome, "factor")
-    expect_is(object@interactions$bin.1, "integer")
-    expect_is(object@interactions$bin.2, "integer")
-    expect_is(object@interactions$condition, "factor")
-    expect_is(object@interactions$replicate, "factor")
-    expect_is(object@interactions$interaction, "numeric")
-    # Keep 0 values before normalisation
-    expect_equal(nrow(object@interactions), 35105)
-    expect_equal(mean(object@interactions$bin.1), 40.0578, tolerance = 1e-4)
-    expect_equal(mean(object@interactions$bin.2), 79.4167, tolerance = 1e-4)
-    expect_equal(mean(object@interactions$interaction), 852.3587,
-                 tolerance = 1e-4)
+    expect_equal(nrow(norm), nrow(object))
+    assay <- SummarizedExperiment::assay(norm)
+    expect_equal(sum(!is.na(assay)), 35105)
+    expect_equal(round(colSums(assay, na.rm=TRUE),2), 
+                 c(1152264.61, 0, 910932.08, 0, 751282.49, 
+                   721601.27, 722004.73))
 })
