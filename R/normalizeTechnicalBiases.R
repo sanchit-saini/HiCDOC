60515ad201bfce27a2c2824c0dc77382e10d76f2
Elise Maigné <elise.maigne@inrae.fr>
2022-01-19 12:18:35 +0100

    correct numerical diff in normalizeTechnical

--- R/normalizeTechnicalBiases.R
+++ R/normalizeTechnicalBiases.R
@@ -60,38 +60,34 @@ normalizeTechnicalBiases <- function(object, parallel = FALSE) {
         hic_table[, .(chr = as.numeric(as.factor(as.character(seqnames1))),
                       region1 = start1,
                       region2 = start2)]
-    hic_table[, D := InteractionSet::pairdist(object, type = "diag")]
+    
     currentAssay <- SummarizedExperiment::assay(object)
     currentAssay[is.na(currentAssay)] <- 0
-    # Reordering columns in alphabetic order (useful for tests)
+    # Reordering columns in condition order
     refOrder <- paste(object$condition, object$replicate)
     currentAssay <- currentAssay[, order(refOrder)]
-    currentAssay <- as.data.table(currentAssay)
-    setnames(currentAssay, paste0("IF", seq_len(ncol(currentAssay))))
     
-    hic_table <- cbind(hic_table, currentAssay)
+    table_list <- lapply(seq_len(ncol(currentAssay)), function(x) cbind(hic_table, currentAssay[,x]))
     
-    table_list <- split(hic_table, hic_table$chr)
+    experiment <-
+        multiHiCcompare::make_hicexp(
+            data_list = table_list,
+            groups = sort(preparedObject$condition),
+            remove_zeros = FALSE,
+            filter = TRUE,
+            zero.p = 1,
+            A.min = 0
+        )
+    normalized <-
+        multiHiCcompare::cyclic_loess(experiment, parallel = parallel)
+    normalized <- multiHiCcompare::hic_table(normalized)
     
-    # plug into parallelized loess function
-    normalized <- .internalApply(
-        parallel = parallel,
-        table_list,
-        FUN = .cloess,
-        iterations = 3,
-        verbose = FALSE,
-        span = NA
-    )
-    normalized <- data.table::rbindlist(normalized)
-    
-    ifcolumns <- colnames(hic_table)[4:ncol(hic_table)]
-    hic_table[, (ifcolumns) := NULL]
+    # Re-sorting the rows in the same order as original
     data.table::setindexv(normalized, c("chr", "region1", "region2"))
     data.table::setindexv(hic_table, c("chr", "region1", "region2"))
     hic_table <- merge(hic_table, normalized, sort = FALSE)
     
-    currentAssay <- hic_table[, 5:ncol(hic_table)]
-    currentAssay <- as.matrix(currentAssay)
+    currentAssay <- as.matrix(hic_table[, 5:ncol(hic_table)])
     # Reordering columns in original order
     currentAssay <- currentAssay[,match(refOrder, sort(refOrder))]
     colnames(currentAssay) <- NULL
