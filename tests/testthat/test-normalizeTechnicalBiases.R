8759f6bb1cc8cea4e171dc7794295077703db00c
Elise Maigné <elise.maigne@inrae.fr>
2022-04-14 13:51:06 +0200

    add test for two different BLAS/LAPACK

--- tests/testthat/test-normalizeTechnicalBiases.R
+++ tests/testthat/test-normalizeTechnicalBiases.R
@@ -10,6 +10,18 @@ test_that("normalizeTechnicalBiases behaves as expected", {
     expect_equal(nrow(norm), nrow(object))
     assay <- SummarizedExperiment::assay(norm)
     expect_equal(sum(!is.na(assay)), 35105)
-    expect_equal(round(colSums(assay, na.rm=TRUE),2), 
-                 c(751294.607, 1152226.832, 0, 0, 721966.582, 721539.165, 910899.793))
+    
+    # Two different possible values, 
+    # 1rst with 
+    # BLAS:   openblas/libblas.so.3
+    # LAPACK: libopenblasp-r0.2.20.so
+    # 2nd with 
+    # Matrix products: default
+    # BLAS:   atlas/libblas.so.3.10.3
+    # LAPACK: atlas/liblapack.so.3.10.
+    expect_true(any(sapply(list(
+        c(751294.6, 1152226.8, 0, 0, 721966.6, 721539.2, 910899.8),
+        c(751294.6, 1152227.4, 0.0, 0.0, 721967.5, 721538.9, 910899.4)), 
+        function(x) identical(x, round(colSums(assay, na.rm=TRUE),1)))))
+    
 })
